2e049690adfd7f3cb40f3f46b893f4e373becdae
Israel Olayinka <israelolayinka459@gmail.com>
2026-07-15 05:49:32 +0100

    chore: add package.json with express dependency

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express()
+const port = 8152;
+const worldCup = [
+    {
+        country:"France",
+        player: "Kylian Mbappe",
+        coach: "Didier Deschamps",
+        number: "10"
+    },
+
+    {
+        country: "brazil",
+        player: "Neymar Jr",
+        coach: "Tite",
+        number: "10"
+    },
+
+    {
+        country: "Argentina",
+        player: "Lionel Messi",
+        coach: "Lionel Scaloni",
+        number: "10"
+    },
+
+    {
+        country: "Germany",
+        player: "Manuel Neuer",
+        coach: "Hansi Flick",
+        number: "1"
+    },
+
+    {
+        country: "Spain",
+        player: "Sergio Ramos",
+        coach: "Luis Enrique",
+        number: "4"
+    }
+
+]
+
+app.get('/home', (req, res) => {
+    res.send('Welcome to Backend Class')
+    console.log('I have accessed the Path');
+    
+})
+
+app.get("/worldcup", (req, res) => {
+    res.send(worldCup)
+    // console.log('I have accessed the Path');
+    
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    
+})
